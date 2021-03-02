98061b435f06ba8f7eecf9e4b092cb2543d76aec
Sandrasanthosh339 <sandrasanthosh339@gmail.com>
2021-03-02 20:56:40 +0530

    first commit

---
+++ Php_cycle/CounterP6.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>php counter</title>
+</head>
+
+<body>
+<?php
+session_start();
+if(isset($_SESSION['counter']))
+{
+ $_SESSION['counter'] += 1;
+}
+else
+{
+ $_SESSION['counter'] = 1;
+} 
+echo "You Have Visited This Page ".$_SESSION['counter']." time in this session ";
+?>
+
+</body>
+</html>
